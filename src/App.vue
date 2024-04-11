231242413c316dd30c6e3c3e5a713bff55069136
Dharma Putra <dharma66putra@gmail.com>
2024-04-11 19:11:12 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const count = ref(0);
+
+const addToCount = () => {
+  count.value++;
+};
+
+const subtractFromCount = () => {
+  console.log("count", count);
+  count.value--;
+};
+</script>
+
+<template>
+  <main>
+    <div>
+      <h4>The curernt count is...</h4>
+      <h1>{{ count }}</h1>
+      <button @click="subtractFromCount">-</button>
+      <button @click="addToCount">+</button>
+    </div>
+  </main>
+</template>
+
+<!-- ? scoped:  -->
+<style scoped>
+main {
+  background-color: aliceblue;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+div {
+  text-align: center;
+}
+</style>
